371427d6bb0e2f270a8da5f4aaab7f694e35931a
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2021-05-09 22:25:48 +0900

    binary der sec

--- src/Address.hs
+++ src/Address.hs
@@ -16,6 +16,15 @@ import GHC.TypeLits ( KnownNat, Nat, natVal )
 import Data.Proxy ( Proxy(..) )
 
 
+import Data.Maybe
+
+import Data.Binary (Binary(..))
+import Data.Binary (encode, decode)
+import Data.Binary.Put (putByteString)
+import Data.Binary.Get (Get(..), getByteString, getBytes)
+import Control.Monad (replicateM)
+
+
 
 
 
@@ -32,10 +41,10 @@ secComp (P (F x) (F y))
     | otherwise = append (pre 3) (toBS x)
 
 
-parseSec :: forall a b p. (KnownNat a, KnownNat b, KnownNat p) => 
+parseSec :: forall a b p. (KnownNat a, KnownNat b, KnownNat p) =>
              ByteString -> Maybe (Point p a b)
 parseSec bs
-    | prefix == 2 = Just $ P x (rootEven y2)     
+    | prefix == 2 = Just $ P x (rootEven y2)
     | prefix == 3 = Just $ P x (rootOdd  y2)
     | prefix == 4 = Just $ P x y
     | otherwise  = Nothing
@@ -44,10 +53,100 @@ parseSec bs
           x = F . fromBS $ B.take 32 b_
           y = F . fromBS $ B.drop 32 b_
           a = F $ natVal (Proxy :: Proxy a)
-          b = F $ natVal (Proxy :: Proxy b) 
+          b = F $ natVal (Proxy :: Proxy b)
           y2 = (x /^/ 3) /+/ (a /*/ x) /+/ b
 
-test p = Just p == parseSec (secComp p)
+instance forall a b p. (KnownNat a, KnownNat b, KnownNat p) => Binary (Point p a b) where
+    put = putByteString . secComp
+    get = do
+        bs <- getByteString 33
+        return . fromJust $ parseSec bs
+
+instance Binary Pubkey where
+    put (Pub p) = put p
+    get = do
+        p <- get :: Get G
+        return $ Pub p
+
+
+
+
+
+
+
+
+
+
+
+
+
+(+++) = append
+
+
+der :: Sig -> ByteString
+der (Sig (F r) (F s)) = prefix +++ len +++ marker +++ rlen +++ rbin +++ marker +++ slen +++ sbin
+    where prefix = pre 4
+          marker = pre 2
+          rbin = mkBin $ toBS r
+          sbin = mkBin $ toBS s
+          mkBin bs = if B.head bs < 128 then bs else  pre 0 +++ bs
+          rlen = pre . toInteger $ B.length rbin
+          slen = pre . toInteger $ B.length sbin
+          len = pre (fromBS rlen + fromBS slen)
+
+check :: ByteString -> Maybe ByteString
+check bs =
+    if B.head bs == 4
+        then return $ B.tail bs
+        else Nothing
+
+getLen :: ByteString -> Maybe (Int, ByteString)
+getLen bs = do
+    let len = fromEnum $ B.head bs
+    let sig = B.tail bs
+    return (len, sig)
+
+isMarked :: ByteString -> Maybe ByteString
+isMarked bs = if B.head bs == 2 then return $ B.tail bs else Nothing
+
+getSig :: ByteString -> Maybe (Int, Fp, ByteString)
+getSig bs = do
+    body <- isMarked bs
+    let len = fromEnum . B.head $ body
+        r = F . fromBS . B.take len . B.tail $ body
+        rest = B.drop (len + 1) body
+    return (len, r,rest)
+
+parseDer :: ByteString -> Maybe Sig
+parseDer bs = do
+    checked <- check bs
+    (len,sig) <- getLen checked
+    (rlen,r,rest) <- getSig sig
+    (slen,s,_) <- getSig rest
+    if len == slen + rlen then return $ Sig r s else Nothing
+
+
+instance Binary Sig where
+    put = putByteString . der
+    get = do
+        c <- getByteString  1 
+        lenb <- getByteString 1
+        let len = fromEnum $ fromBS lenb
+        body <- getByteString (len + 4)
+        let sig = B.append c (B.append lenb body)
+        return .fromJust $ parseDer sig
+
+
+bin = der <$> sigIO
+--test = parseDer <$> bin
+
+test = do
+    sig <- sigIO
+    let r = decode (encode sig)
+    print $ sig == r
+
+--    print (sig == (decode $ encode sig))
+
 
 
 
